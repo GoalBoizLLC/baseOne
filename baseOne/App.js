3211e80949a06a84ddf3fced4e836c4b357d789e
Adam Lukas <86859617+AdamLukas17@users.noreply.github.com>
2023-07-20 08:01:55 -0400

    Starting point template
    Just started up the app so that it displays a thing or two on the screen. Give us something to work with moving forward.

---
+++ baseOne/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import TestButton from './components/TestButton';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Don't press the button!</Text>
+
+      <View style={styles.testButton}>
+        <TestButton label="I am a button" />
+      </View>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  testButton: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
